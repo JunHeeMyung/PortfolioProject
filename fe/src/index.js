40ad2e91dec3eb3b2bb2a039ab41e8b3aabc2b1f
Platinyan <platinyan@naver.com>
2022-12-05 21:01:40 +0900

    fe

---
+++ fe/src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import store from 'store/store'
+import Test from 'components/test/test'
+import Wrapper from 'components/common/wrapper';
+import { Provider } from 'react-redux'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+  <Provider store={store}>
+    <Wrapper>
+      <BrowserRouter>
+        <Test />
+      </BrowserRouter>
+    </Wrapper>
+  </Provider>
+
+);
+reportWebVitals();
