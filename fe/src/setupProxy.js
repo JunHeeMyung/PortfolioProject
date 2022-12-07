603f8271ef984e779d7dc0fca3661a97c22d3c67
Platinyan <platinyan@naver.com>
2022-12-07 20:25:23 +0900

    proxy 변경

--- fe/src/setupProxy.js
+++ fe/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = app => {
     app.use(
         createProxyMiddleware(
-            ['/api','/test'],
+            ['/api',],
         {
             target: process.env.PROXY,
             changeOrigin: true
