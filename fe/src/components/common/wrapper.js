62f59bfa0c939ebe25b04d9926b191f779cd3a43
Platinyan <platinyan@naver.com>
2022-12-18 06:20:50 +0900

    로딩 컴포넌트 추가

--- fe/src/components/common/wrapper.js
+++ fe/src/components/common/wrapper.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axiosInterceptor from "components/common/axiosInterceptor";
 import GlobalStyle from "components/styled/globalStyle";
+import Loading from "components/common/loading";
 
 const Wrapper = props => {
 
@@ -8,6 +9,7 @@ const Wrapper = props => {
 
     return (
         <>
+            <Loading />
             <GlobalStyle backgroundColor='rgb(246,246,246)' color='black'/>
                 {props.children}
         </>
