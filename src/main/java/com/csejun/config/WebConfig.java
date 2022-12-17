44b89073f5354200e7188f83a4bcd6c6612f0bcd
Platinyan <platinyan@naver.com>
2022-12-18 06:20:29 +0900

    cors 설정

--- src/main/java/com/csejun/config/WebConfig.java
+++ src/main/java/com/csejun/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:80","http://localhost:3000")
+            .allowedOrigins("http://localhost:80")
             .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE");
     }
  
