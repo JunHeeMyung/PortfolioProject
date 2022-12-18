537862d4bd1e4bd87e7c6e3de0d53b4863ddb907
Platinyan <platinyan@naver.com>
2022-12-19 04:53:34 +0900

    AxiosSetting 컴포넌트로 분리

--- fe/src/index.js
+++ fe/src/index.js
@@ -6,11 +6,13 @@ import store from 'store/store'
 import Test from 'components/test/test'
 import Wrapper from 'components/common/wrapper';
 import { Provider } from 'react-redux'
+import AxiosSetting from 'components/common/axiosSetting';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <Provider store={store}>
+    <AxiosSetting/>
     <Wrapper>
       <BrowserRouter>
         <Test />
