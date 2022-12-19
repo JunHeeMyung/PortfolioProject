41fe5264b82b661d4d8f023e039a5e39d99e39f5
Platinyan <platinyan@naver.com>
2022-12-20 00:59:29 +0900

    IPHEADER ',' 로 구분된 여러개일경우 처리포함

--- src/main/java/com/csejun/util/IPUtil.java
+++ src/main/java/com/csejun/util/IPUtil.java
@@ -3,46 +3,37 @@ package com.csejun.util;
 import javax.servlet.http.HttpServletRequest;
 
 public class IPUtil {
-    
+	
+	private static final String[] IPHEADER = {
+			"X-Forwarded-For",
+			"Proxy-Client-IP",
+			"HTTP_X_FORWARDED_FOR",
+			"HTTP_X_FORWARDED",
+			"HTTP_X_CLUSTER_CLIENT_IP",
+			"HTTP_CLIENT_IP",
+			"HTTP_FORWARDED_FOR",
+			"HTTP_FORWARDED",
+			"HTTP_VIA",
+			"REMOTE_ADDR"
+	};
+
     public static String getClientIpAddr(HttpServletRequest request) {
     	
-	    String ip = request.getHeader("X-Forwarded-For");
+	    String ip = null;
+	    
+	    for(String ipHeader:IPHEADER) {
+	    	ip = request.getHeader(ipHeader);
+		    if (ip != null && ip.length() != 0 && !"unknown".equalsIgnoreCase(ip)) {
+		    	return getFirstHeaderIp(ip);    
+		    }
+	    }
 	 
-	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-	        ip = request.getHeader("Proxy-Client-IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-	        ip = request.getHeader("WL-Proxy-Client-IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-	        ip = request.getHeader("HTTP_X_FORWARDED_FOR");  
-	    }  
-	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-	        ip = request.getHeader("HTTP_X_FORWARDED");  
-	    }  
-	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-	        ip = request.getHeader("HTTP_X_CLUSTER_CLIENT_IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-	        ip = request.getHeader("HTTP_CLIENT_IP");  
-	    }  
-	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-	        ip = request.getHeader("HTTP_FORWARDED_FOR");  
-	    }  
-	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-	        ip = request.getHeader("HTTP_FORWARDED");  
-	    }  
-	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-	        ip = request.getHeader("HTTP_VIA");  
-	    }  
-	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-	        ip = request.getHeader("REMOTE_ADDR");  
-	    }  
-	    if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {  
-	        ip = request.getRemoteAddr();  
-	    }  
-	 
-	    return ip;
+	    return request.getRemoteAddr();
 	}
-
+    
+    private static String getFirstHeaderIp(String ip) {
+    	
+    	return ip.contains(",")?ip.split(",")[0]:ip;
+    	
+    }
 }
